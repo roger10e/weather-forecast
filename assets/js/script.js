24a5add251136dabe9a4272abc67830ca9042c87
roger10e <rogertenny@gmail.com>
2022-04-06 23:20:49 -0500

    initial files set up

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//There will be a weather dashboard with form inputs
+
+//1. When a user searches for a city, it presents them with current and future conditions for that city
+//1a. That city is also added to the search history
+
+//2. Current weather conditions for a city include: date, icon representation of weather conditions,
+//  temperature, humidity, wind speed, and UV index
+
+//3. The UV index is presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+//4. 5-day forecasts for a presented city displays date, icon representation,
+//  temperature, wind speed, and humidity
+
+//5. Users should be able to click the cities in the search history to be presented with current and future conditions for that city
+
