76e0628e1b0f4a7d1ce1a3a1d4e15d89f21ea252
roger10e <rogertenny@gmail.com>
2022-04-17 21:32:57 -0500

    commented out erroneous code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,9 @@
-//Requirements...
-
-//1. When a user searches for a city, it presents them with current and future conditions for that city
-//1a. That city is also added to the search history
-
-//2. Current weather conditions for a city include: date, icon representation of weather conditions,
-//  temperature, humidity, wind speed, and UV index
-
-//3. The UV index is presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-//4. 5-day forecasts for a presented city displays date, icon representation, temperature, wind speed, and humidity
-
-//5. Users should be able to click the cities in the search history to be presented with current and future conditions for that city
-
-
-//------------------------ re-try
-    //My account & key:
+//My account & key:
         //Username: roger10e / key: b8787de324d36f137022f2eda472ed49
 
     //How to get the correct city upon search ? - longitude & latitude
 
-//variables from another person's code...
+//variables from another person's code, just used for guidance
 // var numOfCities = 9;
 // var citiesListArr = [];
 // var dailyWeatherApiStarts = 'https://api.openweathermap.org/data/2.5/weather?q=';
@@ -41,8 +25,8 @@ btnSearch.click(function(){
     searchLocation(userInput);
     
     //save to localStorage
-    searchHistory.push(userInput);
-    localStorage.setItem("search", JSON.stringify(userInput));
+    // searchHistory.push(userInput);
+    // localStorage.setItem("search", JSON.stringify(userInput));
 })
 
 
@@ -155,11 +139,4 @@ var searchWeather = function (lat, lon) {
     .catch(err => console.error(err));
 }
 
-
-
-
-
-//have to link to jQuery? Don't remember doing this...
-// var searchCityForm = $("#searchCityForm");
-// var searchedCities = $("#searchedCityLi");
 
