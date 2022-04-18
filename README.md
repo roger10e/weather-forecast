0bf291177a61160d80a456b2c6e95bb8e7894522
roger10e <rogertenny@gmail.com>
2022-04-17 21:19:51 -0500

    changed ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # weather-forecast
+
+ ## Table of contents
+* [What is this?](#what-is-this)
+* [Technologies](#technologies)
+* [Functionality](#functionality)
+* [Upcomming Changes](#upcoming-changes)
+* [Contributors](#contributors)
+
+----
+
+## Deployed Page
+[Weather Forecast Dashboard - Deployed Page](https://roger10e.github.io/weather-forecast/)
+
+----
+
+## Technologies Used
+
+* HTML  
+* CSS  
+* Bootstrap
+* Javascript
+* Server-side APIs
+
+----
+
+## Functionality
+
+Users can type in the name of a city and press the search button. If the city is a real city recognized by the API, then the name of the city, along with today's date, will be displayed. 
+
+Along with that data, the day's weather forecast (temp, wind speed, humidity, UV index, and weather icon) will display in the main section.
+
+Below the main section is a series of cards with the five-day forecast for that city.
+
+Searched cities are saved in the sidebar and are able to be clicked to bring their data back into the display.
+    
+----
+
+## Upcoming Changes
+
+* Make sure the saved list is functioning properly
+* Add a double-check just in case the city the user enters is not valid
+* Add other checks, and make the code tighter by instituting loops where appropriate
+
+----
+
+## Contributors
+
+* [@roger10e](https://github.com/roger10e)  
+
+----
--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,15 @@
 //variables from another person's code...
 // var numOfCities = 9;
 // var citiesListArr = [];
-var unit = "units=imperial";
 // var dailyWeatherApiStarts = 'https://api.openweathermap.org/data/2.5/weather?q=';
 // var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
 // var forecastWeatherApiStarts = "https://api.openweathermap.org/data/2.5/onecall?";
-var btnSearch = $("#searchCityForm")
 //http://api.openweathermap.org/geo/1.0/direct?q=krum&limit=5&appid=b8787de324d36f137022f2eda472ed49
+var unit = "units=imperial";
+var btnSearch = $("#searchCityForm")
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-var todayDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+var todayDate = moment().format("MMMM Do YYYY");
 
 
 btnSearch.click(function(){
@@ -40,6 +41,8 @@ btnSearch.click(function(){
     searchLocation(userInput);
     
     //save to localStorage
+    searchHistory.push(userInput);
+    localStorage.setItem("search", JSON.stringify(userInput));
 })
 
 
@@ -51,8 +54,9 @@ var searchLocation = function (input) {
         // console.log(data)
         
         var searchCityName = data[0].local_names.en;
-        console.log(searchCityName);
         $("#cityName").text(searchCityName);
+
+        $("#currentDate").text(todayDate);
 
         //console.log the latitude & longitude
         // console.log(lat);
@@ -92,9 +96,14 @@ var searchWeather = function (lat, lon) {
         $("#UVIndexToday").text(todayUV);
         
         
+        
         //console.log the forecast data object
         console.log(data.daily);
         
+        //set the date for forecast day one
+        var dayOneDate = moment().add(1, 'days').format("MMM Do YY");
+        $("#Date1").text(dayOneDate);
+
         //set the temperature for forecast day one
         var dayOneTemp = data.daily[0].temp.day;
         $("#tempDay1").text(dayOneTemp + " F");
@@ -107,7 +116,8 @@ var searchWeather = function (lat, lon) {
         var dayOneIcon = data.daily[0].weather[0].icon;
         document.getElementById("weatherIconDay1").src = "http://openweathermap.org/img/wn/" + dayOneIcon + "@2x.png";
         
-
+        var dayTwoDate = moment().add(2, 'days').format("MMM Do YY");
+        $("#Date2").text(dayTwoDate);
         var dayTwoTemp = data.daily[1].temp.day;
         $("#tempDay2").text(dayTwoTemp + " F");
         var dayTwoIcon = data.daily[1].weather[0].icon;
@@ -115,7 +125,8 @@ var searchWeather = function (lat, lon) {
         var dayTwoHumidity = data.daily[1].humidity;
         $("#humidityDay2").text(dayTwoHumidity + "%");
         
-        
+        var dayThreeDate = moment().add(3, 'days').format("MMM Do YY");
+        $("#Date3").text(dayThreeDate);
         var dayThreeTemp = data.daily[2].temp.day;
         $("#tempDay3").text(dayThreeTemp + " F");
         var dayThreeHumidity = data.daily[2].humidity;
@@ -123,6 +134,8 @@ var searchWeather = function (lat, lon) {
         var dayThreeIcon = data.daily[2].weather[0].icon;
         document.getElementById("weatherIconDay3").src = "http://openweathermap.org/img/wn/" + dayThreeIcon + "@2x.png";
 
+        var dayFourDate = moment().add(4, 'days').format("MMM Do YY");
+        $("#Date4").text(dayFourDate);
         var dayFourTemp = data.daily[3].temp.day;
         $("#tempDay4").text(dayFourTemp + " F");
         var dayFourHumidity = data.daily[3].humidity;
@@ -130,13 +143,14 @@ var searchWeather = function (lat, lon) {
         var dayFourIcon = data.daily[3].weather[0].icon;
         document.getElementById("weatherIconDay4").src = "http://openweathermap.org/img/wn/" + dayFourIcon + "@2x.png";
 
+        var dayFiveDate = moment().add(5, 'days').format("MMM Do YY");
+        $("#Date5").text(dayFiveDate);
         var dayFiveTemp = data.daily[4].temp.day;
         $("#tempDay5").text(dayFiveTemp + " F");
         var dayFiveHumidity = data.daily[4].humidity;
         $("#humidityDay5").text(dayFiveHumidity + "%");
         var dayFiveIcon = data.daily[4].weather[0].icon;
         document.getElementById("weatherIconDay5").src = "http://openweathermap.org/img/wn/" + dayFiveIcon + "@2x.png";
-
     })
     .catch(err => console.error(err));
 }
