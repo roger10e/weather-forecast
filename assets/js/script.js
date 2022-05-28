ce34afda6f2becaa4d2a1d80304125ad8703dfa2
roger10e <rogertenny@gmail.com>
2022-05-28 15:47:28 -0500

    added UV Index, wind speed, and more

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,8 @@
 
     //How to get the correct city upon search ? - longitude & latitude
 
-//variables from another person's code, just used for guidance
-// var numOfCities = 9;
-// var citiesListArr = [];
-// var dailyWeatherApiStarts = 'https://api.openweathermap.org/data/2.5/weather?q=';
-// var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
-// var forecastWeatherApiStarts = "https://api.openweathermap.org/data/2.5/onecall?";
-//http://api.openweathermap.org/geo/1.0/direct?q=krum&limit=5&appid=b8787de324d36f137022f2eda472ed49
+var citiesListArr = [];
+
 var unit = "units=imperial";
 var btnSearch = $("#searchCityForm")
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
@@ -25,8 +20,8 @@ btnSearch.click(function(){
     searchLocation(userInput);
     
     //save to localStorage
-    // searchHistory.push(userInput);
-    // localStorage.setItem("search", JSON.stringify(userInput));
+    citiesListArr.push(userInput);
+    localStorage.setItem("search", JSON.stringify(citiesListArr));
 })
 
 
@@ -35,9 +30,9 @@ var searchLocation = function (input) {
     .then(response => response.json())
     .then (data => {
         //console.log the data object
-        // console.log(data)
+        console.log(data)
         
-        var searchCityName = data[0].local_names.en;
+        var searchCityName = data[0].name;
         $("#cityName").text(searchCityName);
 
         $("#currentDate").text(todayDate);
@@ -49,9 +44,26 @@ var searchLocation = function (input) {
         var lon = data[0].lon;
         
         searchWeather(lat, lon);
+        saveCities();
     })
     .catch(err => console.error(err));
     
+}
+
+var saveCities = function () {
+    var savedCities = localStorage.getItem("search");
+    console.log(savedCities);
+
+    //clear history list
+
+    //make a for loop to generate button list
+
+        //in for loop append each button to history list
+
+
+
+
+
 }
 
 var searchWeather = function (lat, lon) {
@@ -96,6 +108,14 @@ var searchWeather = function (lat, lon) {
         var dayOneHumidity = data.daily[0].humidity;
         $("#humidityDay1").text(dayOneHumidity + "%");
 
+        //set the uvi for forecast day one
+        var dayOneUVI = data.daily[0].uvi;
+        $("#uviDay1").text(dayOneUVI);
+
+        //set the wind speed for forecast day one
+        var dayOneWind = data.daily[0].wind_speed;
+        $("#windDay1").text(dayOneWind + " mph");
+
         //set the icon for forecast day one
         var dayOneIcon = data.daily[0].weather[0].icon;
         document.getElementById("weatherIconDay1").src = "https://openweathermap.org/img/wn/" + dayOneIcon + "@2x.png";
@@ -108,6 +128,10 @@ var searchWeather = function (lat, lon) {
         document.getElementById("weatherIconDay2").src = "https://openweathermap.org/img/wn/" + dayTwoIcon + "@2x.png";
         var dayTwoHumidity = data.daily[1].humidity;
         $("#humidityDay2").text(dayTwoHumidity + "%");
+        var dayTwoUVI = data.daily[1].uvi;
+        $("#uviDay2").text(dayTwoUVI);
+        var dayTwoWind = data.daily[1].wind_speed;
+        $("#windDay2").text(dayTwoWind + " mph");
         
         var dayThreeDate = moment().add(3, 'days').format("MMM Do YY");
         $("#Date3").text(dayThreeDate);
@@ -115,8 +139,12 @@ var searchWeather = function (lat, lon) {
         $("#tempDay3").text(dayThreeTemp + " F");
         var dayThreeHumidity = data.daily[2].humidity;
         $("#humidityDay3").text(dayThreeHumidity + "%");
+        var dayThreeUVI = data.daily[2].uvi;
+        $("#uviDay3").text(dayThreeUVI);
         var dayThreeIcon = data.daily[2].weather[0].icon;
         document.getElementById("weatherIconDay3").src = "https://openweathermap.org/img/wn/" + dayThreeIcon + "@2x.png";
+        var dayThreeWind = data.daily[2].wind_speed;
+        $("#windDay3").text(dayThreeWind + " mph");
 
         var dayFourDate = moment().add(4, 'days').format("MMM Do YY");
         $("#Date4").text(dayFourDate);
@@ -124,8 +152,12 @@ var searchWeather = function (lat, lon) {
         $("#tempDay4").text(dayFourTemp + " F");
         var dayFourHumidity = data.daily[3].humidity;
         $("#humidityDay4").text(dayFourHumidity + "%");
+        var dayFourUVI = data.daily[3].uvi;
+        $("#uviDay4").text(dayFourUVI);
         var dayFourIcon = data.daily[3].weather[0].icon;
         document.getElementById("weatherIconDay4").src = "https://openweathermap.org/img/wn/" + dayFourIcon + "@2x.png";
+        var dayFourWind = data.daily[3].wind_speed;
+        $("#windDay4").text(dayFourWind + " mph");
 
         var dayFiveDate = moment().add(5, 'days').format("MMM Do YY");
         $("#Date5").text(dayFiveDate);
@@ -133,8 +165,12 @@ var searchWeather = function (lat, lon) {
         $("#tempDay5").text(dayFiveTemp + " F");
         var dayFiveHumidity = data.daily[4].humidity;
         $("#humidityDay5").text(dayFiveHumidity + "%");
+        var dayFiveUVI = data.daily[4].uvi;
+        $("#uviDay5").text(dayFiveUVI);
         var dayFiveIcon = data.daily[4].weather[0].icon;
         document.getElementById("weatherIconDay5").src = "https://openweathermap.org/img/wn/" + dayFiveIcon + "@2x.png";
+        var dayFiveWind = data.daily[4].wind_speed;
+        $("#windDay5").text(dayFiveWind + " mph");
     })
     .catch(err => console.error(err));
 }
